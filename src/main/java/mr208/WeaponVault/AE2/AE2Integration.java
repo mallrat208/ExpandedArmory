882597463d8764be3ff82b4e641d9b19e60ecdf3
mallrat208 <mallrat208@gmail.com>
2014-10-18 01:42:54 -0400

    More Recipes
    Added Recipes for Manasteel and Quartz Weaponry

--- src/main/java/mr208/WeaponVault/AE2/AE2Integration.java
+++ src/main/java/mr208/WeaponVault/AE2/AE2Integration.java
@@ -1,8 +1,10 @@
 package mr208.WeaponVault.AE2;
 
 import appeng.api.AEApi;
+import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.entity.projectile.MaterialRegistry;
 import ckathode.weaponmod.item.*;
+import cpw.mods.fml.common.registry.GameRegistry;
 import mr208.WeaponVault.CustomMaterials;
 import mr208.WeaponVault.Items.WVItemFlail;
 import mr208.WeaponVault.Items.WVItemMelee;
@@ -11,6 +13,8 @@ import mr208.WeaponVault.WVConfig;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import mr208.WeaponVault.Materials;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 
 public class AE2Integration {
@@ -51,10 +55,25 @@ public class AE2Integration {
         musketbayonetQuartz = new WVItemMusket("musketbayonet.quartz", new MeleeCompKnife(Materials.netherQuartz),knifeQuartz);
         spearQuartz = new WVItemMelee("spear.quartz",new MeleeCompSpear(Materials.netherQuartz));
         warhammerQuartz = new WVItemMelee("warhammer.quartz", new MeleeCompWarhammer(Materials.netherQuartz));
+        
+        
+        GameRegistry.addRecipe(new ShapedOreRecipe(battleaxeQuartz, "###", "#X#", " X ", 'X', "stickWood", '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(boomerangQuartz, "XX#", "  X", "  X", 'X', "plankWood", '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(flailQuartz, "  O", " XO", "X #", 'X', "stickWood", 'O', Items.string, '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(halberdQuartz, " ##", " X#", "X  ", 'X', "stickWood", '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(katanaQuartz, "  #", " # ", "X  ", 'X', "stickWood", '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(knifeQuartz, "#X", 'X', "stickWood", '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(knifeQuartz, "#", "X", 'X', "stickWood", '#', Items.quartz));
+        GameRegistry.addShapelessRecipe(new ItemStack(musketbayonetQuartz), knifeQuartz, BalkonsWeaponMod.musket);
+        GameRegistry.addRecipe(new ShapedOreRecipe(spearQuartz, "  #", " X ", "X  ", 'X', "stickWood", '#', Items.quartz));
+        GameRegistry.addRecipe(new ShapedOreRecipe(warhammerQuartz, "#X#", "#X#", " X ", 'X', "stickWood", '#', Items.quartz));
     }
 
     private static void initCertus()
     {
+
+        Item certus = AEApi.instance().materials().materialCertusQuartzCrystal.item();
+
         battleaxeCertus = new WVItemMelee("battleaxe.certus", new MeleeCompBattleaxe(Materials.certusQuartz));
         boomerangCertus = new WVItemMelee("boomerang.certus", new MeleeCompBoomerang(Materials.certusQuartz));
         flailCertus = new WVItemFlail("flail.certus", Materials.certusQuartz);
@@ -64,5 +83,17 @@ public class AE2Integration {
         musketbayonetCertus = new WVItemMusket("musketbayonet.certus", new MeleeCompKnife(Materials.certusQuartz),knifeCertus);
         spearCertus = new WVItemMelee("spear.certus",new MeleeCompSpear(Materials.certusQuartz));
         warhammerCertus = new WVItemMelee("warhammer.certus", new MeleeCompWarhammer(Materials.certusQuartz));
+
+        GameRegistry.addRecipe(new ShapedOreRecipe(battleaxeCertus, "###", "#X#", " X ", 'X', "stickWood", '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(boomerangCertus, "XX#", "  X", "  X", 'X', "plankWood", '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(flailCertus, "  O", " XO", "X #", 'X', "stickWood", 'O', Items.string, '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(halberdCertus, " ##", " X#", "X  ", 'X', "stickWood", '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(katanaCertus, "  #", " # ", "X  ", 'X', "stickWood", '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(knifeCertus, "#X", 'X', "stickWood", '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(knifeCertus, "#", "X", 'X', "stickWood", '#', certus));
+        GameRegistry.addShapelessRecipe(new ItemStack(musketbayonetCertus), knifeCertus, BalkonsWeaponMod.musket);
+        GameRegistry.addRecipe(new ShapedOreRecipe(spearCertus, "  #", " X ", "X  ", 'X', "stickWood", '#', certus));
+        GameRegistry.addRecipe(new ShapedOreRecipe(warhammerCertus, "#X#", "#X#", " X ", 'X', "stickWood", '#', certus));
+        
     }
 }
--- src/main/java/mr208/WeaponVault/Botania/BotaniaIntegration.java
+++ src/main/java/mr208/WeaponVault/Botania/BotaniaIntegration.java
@@ -1,14 +1,20 @@
 package mr208.WeaponVault.Botania;
 
+import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.entity.projectile.MaterialRegistry;
 import ckathode.weaponmod.item.*;
+import cpw.mods.fml.common.registry.GameRegistry;
 import mr208.WeaponVault.Botania.Items.BotanicalItemFlail;
 import mr208.WeaponVault.Botania.Items.BotanicalItemMelee;
 import mr208.WeaponVault.Botania.Items.BotanicalItemMusket;
 import mr208.WeaponVault.CustomMaterials;
 import mr208.WeaponVault.WVConfig;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import vazkii.botania.api.BotaniaAPI;
 import net.minecraft.item.Item;
+import vazkii.botania.common.Botania;
 
 public class BotaniaIntegration {
 
@@ -39,6 +45,17 @@ public class BotaniaIntegration {
         musketbayonetManasteel = new BotanicalItemMusket("musketbayonet.manasteel", new MeleeCompKnife(BotaniaAPI.manasteelToolMaterial),knifeManasteel);
         spearManasteel = new BotanicalItemMelee("spear.manasteel", new MeleeCompSpear(BotaniaAPI.manasteelToolMaterial));
         warhammerManasteel = new BotanicalItemMelee("warhammer.manasteel", new MeleeCompWarhammer(BotaniaAPI.manasteelToolMaterial));
+
+        GameRegistry.addRecipe(new ShapedOreRecipe(battleaxeManasteel, "###", "#X#", " X ", 'X', "livingwoodTwig", '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(boomerangManasteel, "XX#", "  X", "  X", 'X', "plankWood", '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(flailManasteel, "  O", " XO", "X #", 'X', "livingwoodTwig", 'O', Items.string, '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(halberdManasteel, " ##", " X#", "X  ", 'X', "livingwoodTwig", '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(katanaManasteel, "  #", " # ", "X  ", 'X', "livingwoodTwig", '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(knifeManasteel, "#X", 'X', "livingwoodTwig", '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(knifeManasteel, "#", "X", 'X', "livingwoodTwig", '#', "ingotManasteel"));
+        GameRegistry.addShapelessRecipe(new ItemStack(musketbayonetManasteel), knifeManasteel, BalkonsWeaponMod.musket);
+        GameRegistry.addRecipe(new ShapedOreRecipe(spearManasteel, "  #", " X ", "X  ", 'X', "livingwoodTwig", '#', "ingotManasteel"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(warhammerManasteel, "#X#", "#X#", " X ", 'X', "livingwoodTwig", '#', "ingotManasteel"));
 
     }
     private static void initTerrasteel()
